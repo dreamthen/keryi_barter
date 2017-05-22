8c95378c42adf3e6d89a03c44ed942f91fbd684f
尹文楷 <yinwk@servyou.com.cn>
2017-05-22 17:30:42 +0800

    npm and webpack.dll.js rebuild

--- config/webpack.dll.js
+++ config/webpack.dll.js
@@ -1,57 +1,58 @@
 /**
- * Created by yinwk on 2017/5/8.
+ * Created by yinwk on 2017/5/10.
  */
-const path = require("path");
-const webpack = require("webpack");
-//配置dll文件的路径
-//路径巡航
-//相当于:cd __dirname
-//接着:  cd ../dll
-const DLL_DIR = path.resolve(__dirname, "../dll");
-//配置根目录的路径
-const ROOT_DIR = path.resolve(__dirname, "../..");
+//引入webpack打包处理工具外部依赖包
+let webpack = require("webpack");
+//引入path处理路径工具外部依赖包
+let path = require("path");
+//路径巡航,resolve
+//相当于cd __dirname,进入keryi_barter/config路径
+//最后cd ../dir,进入keryi_barter/dll路径,如果不存在,会自动创建一个路径
+let DLL_DIR = path.resolve(__dirname, "../dll");
+let ROOT_DIR = path.resolve(__dirname, "../..");
 
-const keryi_dll_config = {
-    //配置入口文件
+const webpackDll = {
+    //Dll集成管理外部依赖包入口配置
     entry: {
-        vendor: ["react", "react-dom", "react-router", "babel-polyfill"]
+        vendor: ["react", "react-dom", "react-router", "whatwg-fetch", "babel-polyfill"]
     },
-    //配置输出文件格式(文件输出路径、文件名字)
+    //打包输出后,输出的目录路径、文件名、根目录路径和用于创建Dll集成管理外部依赖包的对象
     output: {
-        publicPath: "/",
+        localePath: "/",
         path: DLL_DIR,
         filename: "[name].dll.js",
-        library:"[name]_[chunkhash]"
+        libraryName: "[name]_[chunkhash]"
     },
+    //打包插件,包括错误处理插件、压缩插件、Dll集成外部管理依赖包插件、删除相同或者相似的文件插件
     plugins: [
-        //这个插件可以直接跳过错误,完成打包,防止在打包过程中出现错误,造成源文件损坏
+        //错误处理插件,在打包过程中,为了防止出现错误,而停止打包,导致源文件损坏的插件
         new webpack.NoErrorsPlugin(),
-        //Dll插件,为了避免重复打包外部依赖,如react,react-router等外部依赖,Dll插件会形成一个manifest.dll.json的打包文件api,可以加快打包速度
+        //Dll集成管理外部依赖包插件
         new webpack.DllPlugin({
-            //输出外部依赖包、生成manifest.dll.json文件的路径
+            //生成的集成外部依赖包manifest.dll.json输出目录路径
             path: path.join(DLL_DIR, "[name]_manifest.dll.json"),
-            //生成的dll对象名字,与library相对应
+            //Dll集成管理外部依赖包对象,和output里面的libraryName相对应
             name: "[name]_[chunkhash]",
-            //上下文对象,一般为根目录
+            //上下文对象
             context: ROOT_DIR
         }),
-        //每一次打包都会给每个外部依赖的依赖包分配一个id,id越短的说明越不经常使用,越会靠后打包,可以实现按需加载
+        //给每个打包的外部依赖包分配一个id,根据id的长短进行打包,id越长,说明越经常使用,越靠前被打包,id越短,说明越不常使用,越靠后打包
         new webpack.optimize.OccurrenceOrderPlugin(),
-        //压缩代码插件
+        //压缩插件
         new webpack.optimize.UglifyJsPlugin({
             compress: {
-                //没有用的代码进行压缩
+                //没有用到的代码进行压缩
                 unused: true,
-                //死代码,比如死循环的代码也进行压缩
+                //注释代码或者伪代码进行压缩
                 dead_code: true,
-                //报错或者警告的代码不进行压缩
+                //报出错误或者异常的代码不进行压缩
                 warnings: false
             },
             comments: false
         }),
-        //此插件会在打包外部依赖包的时候,删除重复的或者内容相似的插件
+        //打包时,如果两个文件中含有相同或者相似的内容,会相应进行删除文件内容或者文件
         new webpack.optimize.DedupePlugin()
     ]
 };
 
-module.exports = keryi_dll_config;
+module.exports = webpackDll;
