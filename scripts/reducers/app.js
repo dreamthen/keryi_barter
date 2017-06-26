3bea348b36724965d738e0768dcb4fb2e15e2991
尹文楷 <yinwk@servyou.com.cn>
2017-06-26 16:04:03 +0800

    add functionIcon list

--- scripts/reducers/app.js
+++ scripts/reducers/app.js
@@ -2,10 +2,11 @@
  * Created by yinwk on 2017/6/13.
  */
 import {combineReducers} from "redux";
+import {appReducers} from "./appReducers";
 
 //集成reducers
 const reducers = combineReducers({
-
+    appReducers
 });
 
 //导出集成的reducers
---
+++ scripts/reducers/appReducers.js
@@ -0,0 +1,23 @@
+/**
+ * Created by yinwk on 2017/6/26.
+ */
+import appActionsType from "../actions/appActionsType";
+import insteadState from "../configs/insteadState";
+
+const defaultState = {
+    //对话框标题
+    title: "hello,world",
+    //对话框描述
+    description: "word come true~"
+};
+
+export function appReducers(state = defaultState, actions) {
+    let type = actions.type,
+        newState = actions.payload;
+    switch (type) {
+        case appActionsType["ADD_BARTER_RESOURCE"]:
+            return state;
+        default:
+            return state;
+    }
+}
