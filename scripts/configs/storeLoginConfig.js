55f7622858f2e96164e678853ba3239a6afc2989
dreamThen <2946705425@qq.com>
2017-07-23 14:10:29 +0800

    add login middleware config

--- scripts/configs/storeLoginConfig.js
+++ scripts/configs/storeLoginConfig.js
@@ -1,9 +1,29 @@
 /**
  * Created by yinwk on 2017/6/1.
  */
-import {createStore} from "redux";
+import {createStore, applyMiddleware, compose} from "redux";
+import loggerMiddleware from "redux-logger";
+import thunkMiddleware from "redux-thunk";
 import reducers from "../reducers/login";
 
+const develop = "develop";
+
+let middleware = [];
+//通过DefinePlugin插件添加的功能标识来区分"开发环境"或者"生产环境"该添加或者删除的代码
+if (process.env.NODE_ENV === develop) {
+    middleware = [
+        thunkMiddleware,
+        loggerMiddleware
+    ];
+} else {
+    middleware = [
+        thunkMiddleware
+    ];
+}
+
 //创建react-redux store,将reducers放进createStore中,生成react-redux store
-const store = createStore(reducers);
+const store = createStore(reducers, compose(
+    applyMiddleware(...middleware),
+    window.devToolExtension ? window.devToolExtension : f => f
+));
 export default store;
